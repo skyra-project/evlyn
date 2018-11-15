4a0d9970a5f53363c0c9083af8f4d3041c700ce9
kyranet <kyradiscord@gmail.com>
2018-11-15 04:01:07 +0100

    fix: Main route not working

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,9 +1,12 @@
 import { ServerResponse } from 'http';
-import { KlasaIncomingMessage, Route } from 'klasa-dashboard-hooks';
+import { KlasaIncomingMessage, Route, RouteStore } from 'klasa-dashboard-hooks';
+import { EvlynClient } from '../lib/EvlynClient';
 
 export default class extends Route {
 
-	public route = '/';
+	public constructor(client: EvlynClient, store: RouteStore, file: string[], directory: string) {
+		super(client, store, file, directory, { route: '/' });
+	}
 
 	public async get(_: KlasaIncomingMessage, response: ServerResponse): Promise<void> {
 		response.end(reply);
