c76545d3d4cb97cd537bd05aabe3f2c3c4443c94
Jeroen Claassens <support@favware.tech>
2020-09-08 17:31:22 +0200

    chore: cleanup docker control file

--- control.ps1
+++ control.ps1
@@ -5,19 +5,13 @@ Function Step-Main {
 
     Process {
         switch ( $Command ) {
-            login {
-                $Username = $( Read-Host "Please provide your GitHub username" )
-                $SecurePassword = $( Read-Host -AsSecureString "Please provide a GitHub token with access to publish packages" )
-                ConvertFrom-SecureString -SecureString $SecurePassword | docker login https://docker.pkg.github.com -u $Username --password-stdin
-            }
-            build { yarn clean && yarn build && docker build -t skyrabot/evlyn:main . }
-            run { docker container run -it skyrabot/evlyn:main /bin/sh }
-            deploy { docker push skyrabot/evlyn:main }
-            removeimage { docker rmi -f skyrabot/evlyn:main }
-			remove { docker-compose -p evlyn -f "$($PSScriptRoot)/scripts/docker-compose.yml" rm -fsv $Service }
-			start { docker-compose -p evlyn -f "$($PSScriptRoot)/scripts/docker-compose.yml" up -d $Service }
-			logs { docker-compose -p evlyn -f "$($PSScriptRoot)/scripts/docker-compose.yml" logs $Service }
-			tail { docker-compose -p evlyn -f "$($PSScriptRoot)/scripts/docker-compose.yml" logs -f $Service }
+            build { yarn clean && yarn build && docker build -t evlyn:latest . }
+            run { docker container run -it evlyn:latest /bin/sh }
+            removeimage { docker rmi -f evlyn:latest }
+			remove { docker-compose -p archid -f "$($PSScriptRoot)/scripts/docker-compose.yml" rm -fsv $Service }
+			start { docker-compose -p archid -f "$($PSScriptRoot)/scripts/docker-compose.yml" up -d $Service }
+			logs { docker-compose -p archid -f "$($PSScriptRoot)/scripts/docker-compose.yml" logs $Service }
+			tail { docker-compose -p archid -f "$($PSScriptRoot)/scripts/docker-compose.yml" logs -f $Service }
             default { Write-Host "Unrecognized command, please try again" -ForegroundColor Red }
         }
     }
